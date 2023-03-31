4d37335ecabcd2bafd73cca27511193e7c2cc8f1
Nicholas Hinke <hinke.nicholas@icloud.com>
2023-03-31 22:15:26 +0200

    Initial Commit

---
+++ HTTP Tools/HTTP_ToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HTTP_ToolsApp.swift
+//  HTTP Tools
+//
+//  Created by Nicholas Hinke on 31.03.23.
+//
+
+import SwiftUI
+
+@main
+struct HTTP_ToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HTTP ToolsUITests/HTTP_ToolsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HTTP_ToolsUITestsLaunchTests.swift
+//  HTTP ToolsUITests
+//
+//  Created by Nicholas Hinke on 31.03.23.
+//
+
+import XCTest
+
+final class HTTP_ToolsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
